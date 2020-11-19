172f1cacaf205a1b486917c977e8dab3306ed178
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2020-11-19 17:28:48 +0100

    SecurityDistance Minigame added

---
+++ Assets/Scripts/DragMouse.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class DragMouse : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Vector3 worldPosition;
+
+    private bool moveEnabled;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && moveEnabled)
+        {
+            worldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(worldPosition.x, transform.position.y);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        moveEnabled = true;
+    }
+
+    private void OnMouseUp()
+    {
+        moveEnabled = false;
+    }
+}
---
+++ Assets/Scripts/Objectives/TriggerItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerItem : AObjective
+{
+    [SerializeField] private string activatorTag = "";
+    private bool hovered = false;
+
+    [SerializeField] private Transform player;
+    private Vector3 initPos;
+
+    private void Awake()
+    {
+        initPos = player.position;
+    }
+
+    public override void Reset()
+    {
+        resetPlayer();
+        Completed = false;
+        hovered = false;
+    }
+
+    public override void UpdateState()
+    {
+        if (hovered) Completed = true;
+        else Completed = false;
+    }
+
+    private void resetPlayer()
+    {
+        player.position = initPos;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == activatorTag) hovered = true;
+    }
+}
