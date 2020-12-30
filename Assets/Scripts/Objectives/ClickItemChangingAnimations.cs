6d1c5fe42e2df3db3dd554476568a041e9ae0767
arturo101199 <al375559@uji.es>
2020-12-30 20:31:58 +0100

    Made put minimask and avoid covid minigames

---
+++ Assets/Scripts/Objectives/ClickItemChangingAnimations.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ClickItemChangingAnimations : AObjective
+{
+
+    [SerializeField] Animator anim = null;
+    [SerializeField] string animationTrigger = "";
+
+    public override void ResetObjective()
+    {
+        Completed = false;
+    }
+
+    public override void UpdateState()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Completed = true;
+        if (anim != null) anim.SetTrigger(animationTrigger);
+    }
+}
---
+++ Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    [SerializeField] Vector3 startPos;
+    void Start()
+    {
+        transform.position = startPos;
+    }
+
+    void Update()
+    {
+        
+    }
+}
